8c5e75a4060fd829dca7b8c782645c704e2a2c9c
Jack Evans <jack@evans.gb.net>
2016-12-07 23:12:01 +0000

    nope

---
+++ src/clojure_advent_of_code_2016/day5part2.clj
@@ -0,0 +1,22 @@
+(ns clojure-advent-of-code-2016.day5part1
+ (:require [digest])
+ (:require [clojure.string :as str]))
+
+
+(defn five-zeros? [h]
+  (= (subs h 0 5) "00000"))
+
+(defn hashes
+  ([w] (hashes w 0))
+  ([w n]
+   (lazy-seq
+     (cons (digest/md5 (str w n)) (hashes w (inc n))))))
+
+(defn -main []
+  (->> (hashes "abc")
+       (filter five-zeros?)
+       (take 8)
+       (map #(nth % 5))
+       (apply str)))
+
+(-main)
